759e2353bd9581445b12172d60925fbd2fada4d9
Diestefano17 <82546778+Diestefano17@users.noreply.github.com>
2022-10-10 15:28:44 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numero = 3;
+        do {
+            System.out.println(numero);
+            numero = numero +1;
+        } while(numero <= 5);
+        System.out.println("Terminado!!");
+
+    }
+}
